1882df24cf3dde8d086fdb3365ea60e0032da712
alejod <dedominicci.alejo@gmail.com>
2023-10-02 21:19:09 -0300

    Segunda Preentrega

--- main.js
+++ main.js
@@ -1,61 +1,89 @@
-alert("Ingrese la opcion del producto que desea llevar, para salir ingrese 0")
-let seleccionarProductos = Number(prompt("1-Buzo($8000)\n2-Remera($5000)\n3-Pantalon($8500)\n4-Zapatilla($11000)"))
-let seleccionarCantidad
-let total = 0
+alert("Ingrese la opción del producto que desea llevar. Para salir, ingrese 0.");
 
-const cantidad = (cant, precio) =>{
-    return cant * precio
+const productos = [
+  { nombre: "Buzo", precio: 8000 },
+  { nombre: "Remera", precio: 5000 },
+  { nombre: "Pantalón", precio: 8500 },
+  { nombre: "Zapatilla", precio: 11000 },
+];
+
+let carrito = [];
+let total = 0;
+
+const agregarAlCarrito = (producto, cantidad) => {
+  carrito.push({ ...producto, cantidad });
+};
+
+const calcularSubtotal = (item) => {
+  return item.cantidad * item.precio;
+};
+
+const mostrarCarrito = () => {
+  alert("Carrito de compras:\n" + JSON.stringify(carrito, null, 2));
+};
+
+const mostrarTotal = () => {
+  alert("El total de la compra es de: $" + total);
+};
+
+while (true) {
+  let seleccionarProductos = Number(
+    prompt(
+      "1-Buzo($8000)\n2-Remera($5000)\n3-Pantalón($8500)\n4-Zapatilla($11000)\n0-Salir"
+    )
+  );
+
+  if (seleccionarProductos === 0) {
+    break; // Salir del bucle si el usuario selecciona 0
+  }
+
+  if (seleccionarProductos < 1 || seleccionarProductos > 4) {
+    alert("Opción no válida. Por favor, elija una opción válida.");
+    continue; // Volver al inicio del bucle
+  }
+
+  let cantidadSeleccionada = Number(prompt("Indique la cantidad"));
+
+  if (cantidadSeleccionada <= 0 || isNaN(cantidadSeleccionada)) {
+    alert("Cantidad no válida. Por favor, ingrese una cantidad válida.");
+    continue; // Volver al inicio del bucle
+  }
+
+  const productoSeleccionado = productos[seleccionarProductos - 1];
+  agregarAlCarrito(productoSeleccionado, cantidadSeleccionada);
 }
 
-while (seleccionarProductos != 0) {
-    switch (seleccionarProductos) {
-        case 1:
-                seleccionarCantidad = Number(prompt("El producto seleccionado es buzo, indique la cantidad"))
-                total += cantidad(seleccionarCantidad, 8000)
-            break;
-        case 2:
-                seleccionarCantidad = Number(prompt("El producto seleccionado es remera, indique la cantidad"))
-                total += cantidad(seleccionarCantidad, 5000)
-            break;
-        case 3:
-                seleccionarCantidad = Number(prompt("El producto seleccionado es pantalon, indique la cantidad"))
-                total += cantidad(seleccionarCantidad, 8500)
-            break;
-        case 4:
-                seleccionarCantidad = Number(prompt("El producto seleccionado es zapatilla, indique la cantidad"))
-                total += cantidad(seleccionarCantidad, 11000)
-            break;
-    
-        default:
-            break;
-    }
-    seleccionarProductos = Number(prompt("1-Buzo($8000)\n2-Remera($5000)\n3-Pantalon($8500)\n4-Zapatilla($11000)"))
+if (carrito.length === 0) {
+  alert("El carrito está vacío.");
+} else {
+  mostrarCarrito();
+  total = carrito.reduce((acc, item) => acc + calcularSubtotal(item), 0);
+  mostrarTotal();
 }
 
-alert("El total de la compra es de: $" + total)
+const calcularCostoEnvio = () => {
+  if (total >= 20000) {
+    alert("El envío es gratuito.");
+  } else {
+    total += 1000;
+    alert("El costo de envío es de 1000, el total es: $" + total);
+  }
+};
 
-const envio = () => {
-    if (total >= 20000){
-        alert("El envio es gratuito")
-    }else{
-        total += 1000
-        alert("El costo de envio es de 1000, el total es: $" + total)
-    }
-}
+calcularCostoEnvio();
 
-envio()
+const procesarMetodoDePago = () => {
+  let metodo = prompt("Ingrese el método de pago: tarjeta o efectivo");
 
-const metodoDePago = () => {
-    let metodo = prompt("Ingrese el metodo de pago: tarjeta o efectivo" )
-    if (metodo == "tarjeta") {
-      total *= 1.1
-      alert("El total es de: $" + total)
-    }else if ( metodo == "efectivo") {
-      total -= 1000
-      alert("Tenes un descuento de 1000, el total es: $" + total)
-    }  
-}
-  
-metodoDePago()
+  if (metodo === "tarjeta") {
+    total *= 1.1;
+    alert("El total es de: $" + total);
+  } else if (metodo === "efectivo") {
+    total -= 1000;
+    alert("Tiene un descuento de 1000, el total es: $" + total);
+  }
+};
 
-alert("Muchas gracias por su compra, que lo disfrutes :)")
+procesarMetodoDePago();
+
+alert("¡Gracias por su compra! ¡Que lo disfrute! :)");
