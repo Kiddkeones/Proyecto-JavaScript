a976597ccc47fc3514320a84d7c2b4a490c21e15
alejod <dedominicci.alejo@gmail.com>
2023-11-09 20:21:17 -0300

    Proyecto final finalizado :)

--- src/js/btn-product.js
+++ src/js/btn-product.js
@@ -1,7 +1,21 @@
 export const addToCartBtn = () => {
     const addCartButtons = document.querySelectorAll(".add-cart-btn");
+    
     addCartButtons.forEach((button, index) => {
         button.addEventListener("click", () => {
+            Toastify({
+                text: "Añadido al Carrito",
+                duration: 1000,
+                gravity: "top", // `top` or `bottom`
+                position: "left", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "linear-gradient(to right, #111, #111)",
+                  borderRadius: "2rem"
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+
             const productCard = button.parentNode;
             const title =
                 productCard.querySelector(".title-tarjetita").innerText;
