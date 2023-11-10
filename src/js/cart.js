c7e7f4a50cc80edd78274d33d64ee2a35e26a872
alejod <dedominicci.alejo@gmail.com>
2023-11-09 21:02:42 -0300

    Agregue funcion Realizar compra y Proyecto Finalizado

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,75 +1,99 @@
 //Codigo de pages/cart.html
 const cart = document.getElementById("cart");
 const productsFromLocalStorage = JSON.parse(localStorage.getItem("cart"));
-
+ 
 let total = 0;
 
 for (const key in productsFromLocalStorage) {
-    if (productsFromLocalStorage.hasOwnProperty(key)) {
-        const product = productsFromLocalStorage[key];
+  if (productsFromLocalStorage.hasOwnProperty(key)) {
+    const product = productsFromLocalStorage[key];
 
-        const productDiv = document.createElement("div");
-        productDiv.classList.add("product");
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
 
-        //~> Container
-        const productDetails = document.createElement("div");
-        productDetails.classList.add("product-details");
+    //~> Container
+    const productDetails = document.createElement("div");
+    productDetails.classList.add("product-details");
 
-        //~> Name
-        const productName = document.createElement("p");
-        productName.classList.add("product-name");
-        productName.textContent = product.title;
+    //~> Name
+    const productName = document.createElement("p");
+    productName.classList.add("product-name");
+    productName.textContent = product.title;
 
-        //~> Price
-        const productPrice = document.createElement("p");
-        productPrice.classList.add("product-price");
-        productPrice.textContent = product.price;
+    //~> Price
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("product-price");
+    productPrice.textContent = product.price;
 
-        //~> Quantity
-        const productQuantity = document.createElement("p");
-        productQuantity.classList.add("product-quantity");
+    //~> Quantity
+    const productQuantity = document.createElement("p");
+    productQuantity.classList.add("product-quantity");
+    productQuantity.textContent = `x ${product.quantity}`;
+
+    const compraButton = document.createElement("button");
+    compraButton.classList.add("compra-btn");
+    compraButton.textContent = "Realizar Compra"
+    compraButton.addEventListener("click", () => {
+      realizarCompra();
+    });
+
+
+    //~> Delete Button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.textContent = "Remove";
+    deleteButton.addEventListener("click", () => {
+      const updatedCart = JSON.parse(localStorage.getItem("cart"));
+      const product = updatedCart[key];
+
+      if (product.quantity > 1) {
+        product.quantity--;
         productQuantity.textContent = `x ${product.quantity}`;
+      } else {
+        updatedCart[key] = undefined;
 
-        //~> Delete Button
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete-btn");
-        deleteButton.textContent = "Remove";
-        deleteButton.addEventListener("click", () => {
-            const updatedCart = JSON.parse(localStorage.getItem("cart"));
-            const product = updatedCart[key];
+        productDiv.remove();
+      }
 
-            if (product.quantity > 1) {
-                product.quantity--;
-                productQuantity.textContent = `x ${product.quantity}`;
-            } else {
-                updatedCart[key] = undefined;
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
 
-                productDiv.remove();
-            }
+      total -= parseFloat(product.price.replace("$", "").replace(",", ""));
+      totalElement.innerHTML = `<p>Total: $${total.toFixed(2)}</p>`;
+    });
 
-            localStorage.setItem("cart", JSON.stringify(updatedCart));
+    productDetails.appendChild(compraButton);
+    productDetails.appendChild(productName);
+    productDetails.appendChild(productPrice);
+    productDetails.appendChild(productQuantity);
+    productDetails.appendChild(deleteButton);
 
-            total -= parseFloat(
-                product.price.replace("$", "").replace(",", "")
-            );
-            totalElement.innerHTML = `<p>Total: $${total.toFixed(2)}</p>`;
-        });
+    productDiv.appendChild(productDetails);
 
-        productDetails.appendChild(productName);
-        productDetails.appendChild(productPrice);
-        productDetails.appendChild(productQuantity);
-        productDetails.appendChild(deleteButton);
+    cart.insertBefore(productDiv, cart.querySelector(".total"));
 
-        productDiv.appendChild(productDetails);
-
-        cart.insertBefore(productDiv, cart.querySelector(".total"));
-
-        const price = parseFloat(
-            product.price.replace("$", "").replace(",", "")
-        );
-        total += price * product.quantity;
-    }
+    const price = parseFloat(product.price.replace("$", "").replace(",", ""));
+    total += price * product.quantity;
+  }
 }
 
 const totalElement = cart.querySelector(".total");
 totalElement.innerHTML = `<p>Total: $${total.toFixed(2)}</p>`;
+
+// Función para ejecutar cuando se hace clic en algo (por ejemplo, un botón)
+function realizarCompra() {
+  // Borrar el almacenamiento local
+  localStorage.clear();
+  const product = cart.querySelector(".product");
+  product.innerHTML = " "
+  const totalElement = cart.querySelector(".total");
+totalElement.innerHTML = `<p>Total: $0</p>`;
+
+  // Mostrar la alerta de agradecimiento
+  Swal.fire({
+    title: "¡Gracias por tu compra!",
+    text: "Esperamos que disfrutes de tu producto.",
+    icon: "success",
+    confirmButtonText: "Aceptar",
+  });
+}
+
